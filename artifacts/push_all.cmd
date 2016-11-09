22821a61b9d64b145873c2d837cfe00831c95683
Karl Waclawek <karl.waclawek@qlinesolutions.com>
2016-11-09 09:02:29 -0500

    [Solution] New Nuget.exe, new MyFeed URL.

--- artifacts/push_all.cmd
+++ artifacts/push_all.cmd
@@ -1,3 +1,3 @@
-For /R %%G IN (*.nupkg) do nuget push "%%G" -Source "http://40.79.87.36/MyFeed/nuget" -ApiKey "DB420C35-E51D-4B1C-A5F5-FF73732683B9"
+For /R %%G IN (*.nupkg) do nuget push "%%G" -Source "http://40.123.48.16/MyFeed/api/v2/package" -ApiKey "DB420C35-E51D-4B1C-A5F5-FF73732683B9"
 
 pause
