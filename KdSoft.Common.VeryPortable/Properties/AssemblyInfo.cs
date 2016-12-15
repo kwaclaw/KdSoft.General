6429d5d21425673c64a9ca7a75e6f465f3d75271
Karl Waclawek <karl.waclawek@qlinesolutions.com>
2016-12-15 14:26:04 -0500

    Added Nuget meta-data and SharedAssemblyInfo.cs. Rebuilt and republished.

---
+++ KdSoft.Common.VeryPortable/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("KdSoft.Common.VeryPortable")]
+
+#if !COREFX
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("06B71F43-6FF0-434E-B054-CD12123938F0")]
+#endif
--- Projects_Pack.cmd
+++ Projects_Pack.cmd
@@ -1,13 +1,16 @@
 
-dotnet pack -o artifacts KdSoft.Reflection
-dotnet pack -o artifacts KdSoft.CodeConfig
-dotnet pack -o artifacts KdSoft.Common.VeryPortable
-dotnet pack -o artifacts KdSoft.Reflection.Portable
-dotnet pack -o artifacts KdSoft.StorageBase
-dotnet pack -o artifacts KdSoft.TransientStorage
-dotnet pack -o artifacts KdSoft.Utils
-dotnet pack -o artifacts KdSoft.Utils.Portable
-REM dotnet pack -o artifacts KdSoft.Utils.VeryPortable
+DEL artifacts\*.nupkg
+
+nuget pack -o artifacts KdSoft.Reflection\project.json
+nuget pack -o artifacts KdSoft.CodeConfig\project.json
+nuget pack -o artifacts KdSoft.Common.VeryPortable\project.json
+nuget pack -o artifacts KdSoft.Reflection.Portable\project.json
+nuget pack -o artifacts KdSoft.StorageBase\project.json
+nuget pack -o artifacts KdSoft.TransientStorage\project.json
+nuget pack -o artifacts KdSoft.Utils\project.json
+nuget pack -o artifacts KdSoft.Utils.Portable\project.json
+REM nuget pack -o artifacts KdSoft.Utils.VeryPortable\project.json
+nuget pack -o artifacts KdSoft.Quartz.Jobs\project.json
 
 pause
 
