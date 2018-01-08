90db90d2ce2575dada708942adc648a5afc4ff1d
Karl Waclawek <karl@waclawek.net>
2018-01-08 10:19:24 -0500

    Solution and project level cleanup.

--- KdSoft.Reflection/ReflectUtils.cs
+++ KdSoft.Reflection/ReflectUtils.cs
@@ -13,45 +13,46 @@ using System.Security.Policy;
 
 namespace KdSoft.Reflection
 {
-    /// <summary>
-    /// Reflection helper routines.
-    /// </summary>
+  /// <summary>
+  /// Reflection helper routines.
+  /// </summary>
 #if !COREFX
     [SecuritySafeCritical, PermissionSet(SecurityAction.Assert, Unrestricted = true)]
 #endif
-    public static class ReflectUtils
-    {
-        /// <summary>
-        /// Extracts the simple assembly name from an assembly's display name.
-        /// </summary>
-        /// <param name="assemblyDisplayName">Assembly display name to extract from.</param>
-        /// <returns>Extracted simple name.</returns>
-        public static string ExtractSimpleName(string assemblyDisplayName) {
-            if (string.IsNullOrEmpty(assemblyDisplayName))
-                return assemblyDisplayName;
-            int commaIndx = assemblyDisplayName.IndexOf(',');
-            if (commaIndx < 0)
-                return assemblyDisplayName;
-            else
-                return assemblyDisplayName.Substring(0, commaIndx);
-        }
+  public static class ReflectUtils
+  {
+    /// <summary>
+    /// Extracts the simple assembly name from an assembly's display name.
+    /// </summary>
+    /// <param name="assemblyDisplayName">Assembly display name to extract from.</param>
+    /// <returns>Extracted simple name.</returns>
+    public static string ExtractSimpleName(string assemblyDisplayName) {
+      if (string.IsNullOrEmpty(assemblyDisplayName))
+        return assemblyDisplayName;
+      int commaIndx = assemblyDisplayName.IndexOf(',');
+      if (commaIndx < 0)
+        return assemblyDisplayName;
+      else
+        return assemblyDisplayName.Substring(0, commaIndx);
+    }
 
-        /// <summary>
-        /// Finds assemblies in the <see cref="DependencyContext"/> of an assembly, given an assembly "simple name".
-        /// The comparison is not case-sensitive.
-        /// </summary>
-        /// <param name="deps">DependencyContext to search.</param>
-        /// <param name="matchName">Simple name of the assembly, to match against.</param>
-        /// <returns>List of <see cref="AssemblyName"/> instances.</returns>
-        public static IList<AssemblyName> FindAssemblies(this DependencyContext deps, string matchName) {
-            var result = new List<AssemblyName>();
-            foreach (var assName in deps.GetRuntimeAssemblyNames(RuntimeEnvironment.GetRuntimeIdentifier())) {
-                if (string.Compare(assName.Name, matchName, StringComparison.OrdinalIgnoreCase) == 0) {
-                    result.Add(assName);
-                }
-            }
-            return result;
+    /// <summary>
+    /// Finds assemblies in the <see cref="DependencyContext"/> of an assembly, given an assembly "simple name".
+    /// The comparison is not case-sensitive.
+    /// </summary>
+    /// <param name="deps">DependencyContext to search.</param>
+    /// <param name="matchName">Simple name of the assembly, to match against.</param>
+    /// <returns>List of <see cref="AssemblyName"/> instances.</returns>
+    [CLSCompliant(false)]
+    public static IList<AssemblyName> FindAssemblies(this DependencyContext deps, string matchName) {
+      var result = new List<AssemblyName>();
+      foreach (var assName in deps.GetRuntimeAssemblyNames(RuntimeEnvironment.GetRuntimeIdentifier())) {
+        if (string.Compare(assName.Name, matchName, StringComparison.OrdinalIgnoreCase) == 0) {
+          result.Add(assName);
         }
+      }
+      return result;
+    }
 
 #if !COREFX
         /// <summary>
@@ -120,71 +121,71 @@ namespace KdSoft.Reflection
         }
 #endif
 
-        /// <summary>
-        /// Finds all classes that derive from a given class or implement a given interface.
-        /// </summary>
-        /// <param name="assembly">Assembly to search.</param>
-        /// <param name="superType">Super type to search for in classes.</param>
-        /// <param name="publicOnly">Indicates if only public types should be searched.</param>
-        /// <returns>List of classes that derive from the given class or that implement the given interface.</returns>
-        public static List<Type> FindSubClasses(this Assembly assembly, Type superType, bool publicOnly) {
-            List<Type> result = new List<Type>();
-            Type[] types;
-            if (publicOnly)
-                types = assembly.GetExportedTypes();
-            else
-                types = assembly.GetTypes();
-            foreach (Type tp in types) {
+    /// <summary>
+    /// Finds all classes that derive from a given class or implement a given interface.
+    /// </summary>
+    /// <param name="assembly">Assembly to search.</param>
+    /// <param name="superType">Super type to search for in classes.</param>
+    /// <param name="publicOnly">Indicates if only public types should be searched.</param>
+    /// <returns>List of classes that derive from the given class or that implement the given interface.</returns>
+    public static List<Type> FindSubClasses(this Assembly assembly, Type superType, bool publicOnly) {
+      List<Type> result = new List<Type>();
+      Type[] types;
+      if (publicOnly)
+        types = assembly.GetExportedTypes();
+      else
+        types = assembly.GetTypes();
+      foreach (Type tp in types) {
 #if COREFX
-                if (tp.GetTypeInfo().IsClass && superType.IsAssignableFrom(tp))
+        if (tp.GetTypeInfo().IsClass && superType.IsAssignableFrom(tp))
 #else
                     if (tp.IsClass && superType.IsAssignableFrom(tp))
 #endif
-                    result.Add(tp);
-            }
-            return result;
-        }
+          result.Add(tp);
+      }
+      return result;
+    }
 
-        /// <summary>
-        /// Returns type objects for a given set of object instances. If an instance reference is <c>null</c>,
-        /// the type <see cref="object"/> is assumed.
-        /// </summary>
-        /// <param name="objs">Object instances to return types for. Can be <c>null</c>.</param>
-        /// <returns>Array of <see cref="Type"/> instances, which can be empty, but will not be <c>null</c>.</returns>
-        public static Type[] GetTypes(params object[] objs) {
-            if (objs == null)
-                return new Type[] { typeof(object) };
-            Type[] result = new Type[objs.Length];
-            for (int indx = 0; indx < result.Length; indx++) {
-                object obj = objs[indx];
-                if (obj != null)
-                    result[indx] = obj.GetType();
-                else
-                    result[indx] = typeof(object);
-            }
-            return result;
-        }
+    /// <summary>
+    /// Returns type objects for a given set of object instances. If an instance reference is <c>null</c>,
+    /// the type <see cref="object"/> is assumed.
+    /// </summary>
+    /// <param name="objs">Object instances to return types for. Can be <c>null</c>.</param>
+    /// <returns>Array of <see cref="Type"/> instances, which can be empty, but will not be <c>null</c>.</returns>
+    public static Type[] GetTypes(params object[] objs) {
+      if (objs == null)
+        return new Type[] { typeof(object) };
+      Type[] result = new Type[objs.Length];
+      for (int indx = 0; indx < result.Length; indx++) {
+        object obj = objs[indx];
+        if (obj != null)
+          result[indx] = obj.GetType();
+        else
+          result[indx] = typeof(object);
+      }
+      return result;
+    }
 
-        /// <summary>
-        /// Returns the singleton instance for a given class type, if it exists.
-        /// The instance must be exposed through a public static property or field with the specified name.
-        /// </summary>
-        /// <param name="type">Type to get singleton instance for.</param>
-        /// <param name="name">Name for public static property or field. String comparison is *not* case sensitive.</param>
-        /// <returns>Singleton instance, or <c>null</c> if no matching property or field is found.</returns>
+    /// <summary>
+    /// Returns the singleton instance for a given class type, if it exists.
+    /// The instance must be exposed through a public static property or field with the specified name.
+    /// </summary>
+    /// <param name="type">Type to get singleton instance for.</param>
+    /// <param name="name">Name for public static property or field. String comparison is *not* case sensitive.</param>
+    /// <returns>Singleton instance, or <c>null</c> if no matching property or field is found.</returns>
 #if COREFX
-        public static object GetSingletonInstance(this Type type, string name) {
-            if (!type.GetTypeInfo().IsClass)
-                throw new ArgumentException(string.Format("Singleton type '{0}' must be class.", type.FullName));
-            BindingFlags bfs = BindingFlags.IgnoreCase | BindingFlags.Static | BindingFlags.Public;
-            PropertyInfo propInfo = type.GetProperty(name, bfs);
-            if (propInfo != null)
-                return propInfo.GetValue(null, null);
-            FieldInfo fldInfo = type.GetField(name, bfs);
-            if (fldInfo != null && fldInfo.FieldType == type)
-                return fldInfo.GetValue(null);
-            return null;
-        }
+    public static object GetSingletonInstance(this Type type, string name) {
+      if (!type.GetTypeInfo().IsClass)
+        throw new ArgumentException(string.Format("Singleton type '{0}' must be class.", type.FullName));
+      BindingFlags bfs = BindingFlags.IgnoreCase | BindingFlags.Static | BindingFlags.Public;
+      PropertyInfo propInfo = type.GetProperty(name, bfs);
+      if (propInfo != null)
+        return propInfo.GetValue(null, null);
+      FieldInfo fldInfo = type.GetField(name, bfs);
+      if (fldInfo != null && fldInfo.FieldType == type)
+        return fldInfo.GetValue(null);
+      return null;
+    }
 #else
         public static object GetSingletonInstance(this Type type, string name) {
             if (!type.IsClass)
@@ -227,7 +228,7 @@ namespace KdSoft.Reflection
             }
         }
 #endif
-    }
+  }
 
 #if !COREFX
     /// <summary>
--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
@@ -6,6 +7,8 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+[assembly: CLSCompliant(true)]
+
 
 #if !COREFX
 // Setting ComVisible to false makes the types in this assembly not visible
