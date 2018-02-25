cc3ae389f53940cb04a557bee2d7d95d83b5676d
Karl Waclawek <karl@waclawek.net>
2018-02-25 14:14:31 -0500

    [KdSoft.Utils.Portable] Added ObjectEqualityComparer class to enable tracking objects by their reference even if they override Equals() and GetHashCode().

---
+++ KdSoft.Utils.Portable/ObjectEqualityComparer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+namespace KdSoft.Utils.Portable
+{
+  /// <summary>
+  /// Implementation of <see cref="IEqualityComparer{T}"/> that compares objects
+  /// by reference and calculates their hash code based on the reference only.
+  /// This is useful when objects need to be tracked based on their object handle
+  /// even if <see cref="Object.Equals(object)"/> and <see cref="Object.GetHashCode"/>
+  /// have been overridden to implement value type semantics. 
+  /// </summary>
+  public class ObjectEqualityComparer : IEqualityComparer<object>
+  {
+    /// <inheritdoc />
+    public new bool Equals(object x, object y) {
+      return object.ReferenceEquals(x, y);
+    }
+
+    /// <inheritdoc />
+    public int GetHashCode(object obj) {
+      return RuntimeHelpers.GetHashCode(obj);
+    }
+  }
+
+}
