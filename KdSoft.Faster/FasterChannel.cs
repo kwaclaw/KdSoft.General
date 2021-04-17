6425e7bd7f9e2722025c991b47db782147b8a64c
Karl Waclawek <karl@waclawek.net>
2021-04-17 16:00:27 -0400

    Added KdSoft.Faster library.

---
+++ KdSoft.Faster/FasterChannel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+using FASTER.core;
+
+namespace KdSoft.Faster
+{
+  /// <summary>
+  /// A persistent queue, similar to <see cref="Channel"/>.
+  /// Uses <see cref="FasterLog"/> as the storage mechanism.
+  /// </summary>
+  public class FasterChannel: IDisposable
+  {
+    readonly FasterLog _log;
+    readonly IDevice _device;
+    long _logicalAddress;
+
+    public FasterChannel(string deviceLogPath) {
+      _device = Devices.CreateLogDevice(deviceLogPath);
+      _log = new FasterLog(new FasterLogSettings { LogDevice = _device });
+    }
+
+    public bool TryWrite(ReadOnlyMemory<byte> item) {
+      return _log.TryEnqueue(item.Span, out _logicalAddress);
+    }
+
+    public async ValueTask WriteAsync(ReadOnlyMemory<byte> item, CancellationToken cancellationToken = default) {
+      _logicalAddress = await _log.EnqueueAsync(item, cancellationToken).ConfigureAwait(false);
+    }
+
+    public FasterReader GetNewReader() {
+      return new FasterReader(_log);
+    }
+
+    public void Commit(bool spinWait = false) {
+      _log.Commit(spinWait);
+    }
+
+    public ValueTask CommitAsync(CancellationToken cancellationToken = default) {
+      return _log.CommitAsync(cancellationToken);
+    }
+
+    public void Dispose() {
+      _log.Dispose();
+      _device.Dispose();
+    }
+  }
+}
---
+++ KdSoft.Faster/FasterReader.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Buffers;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+using FASTER.core;
+
+namespace KdSoft.Faster
+{
+  /// <summary>
+  /// Helper routines to iterate over a <see cref="FasterLog"/> in a channel-like way.
+  /// </summary>
+  public class FasterReader: IDisposable
+  {
+    readonly FasterLog _log;
+    readonly FasterLogScanIterator _iter;
+    readonly MemoryPool<byte> _pool;
+
+    long _nextAddress;
+
+    public FasterReader(FasterLog log) {
+      this._log = log;
+      _nextAddress = log.BeginAddress;
+      _iter = log.Scan(0, long.MaxValue, "channel");
+      _pool = MemoryPool<byte>.Shared;
+    }
+
+    public bool TryRead([MaybeNullWhen(false)] out IMemoryOwner<byte> item, out int entryLength) {
+      if (_iter.GetNext(_pool, out item, out entryLength, out var currentAddress, out _nextAddress)) {
+        return true;
+      }
+      item = null;
+      return false;
+    }
+
+    public async ValueTask<(IMemoryOwner<byte>, int)> ReadAsync(CancellationToken cancellationToken = default) {
+      while (true) {
+        if (!await WaitToReadAsync(cancellationToken).ConfigureAwait(false))
+          throw new ChannelClosedException();
+
+        if (TryRead(out IMemoryOwner<byte>? item, out int entryLength))
+          return (item, entryLength);
+      }
+    }
+
+    // this approach completes the iteration wheren there is no more data to read and does not wait for the next commit
+    public async IAsyncEnumerable<(IMemoryOwner<byte>, int)> ReadAllAsync([EnumeratorCancellation] CancellationToken cancellationToken = default) {
+      await foreach ((IMemoryOwner<byte> entry, var entryLength, var currentAddress, var nextAddress) in _iter.GetAsyncEnumerable(_pool, cancellationToken)) {
+        _nextAddress = nextAddress;
+        yield return (entry, entryLength);
+
+        // MoveNextAsync() would hang at TailAddress, waiting for more entries (that we don't add).
+        // Note: If this happens and the test has to be canceled, there may be a leftover blob from the log.Commit(), because
+        // the log device isn't Dispose()d; the symptom is currently a numeric string format error in DefaultCheckpointNamingScheme.
+        if (nextAddress == _log.TailAddress)
+          break;
+      }
+    }
+
+    // this approach will wait for the next commit
+    public IAsyncEnumerable<(IMemoryOwner<byte> entry, int entryLength, long currentAddress, long nextAddress)> GetAsyncEnumerable(CancellationToken token = default) {
+      return _iter.GetAsyncEnumerable(_pool, token);
+    }
+
+    public ValueTask<bool> WaitToReadAsync(CancellationToken cancellationToken = default) {
+      return _iter.WaitAsync(cancellationToken);
+    }
+
+    public void Truncate() {
+      _log.TruncateUntil(_nextAddress);
+    }
+
+    public void Dispose() {
+      _iter.Dispose();
+      _pool.Dispose();
+    }
+  }
+}
